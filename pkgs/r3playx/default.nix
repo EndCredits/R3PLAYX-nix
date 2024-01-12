8b8d368eed1a7c74d5e6b2130a243b70fc96cc6b
EndCredits <endcredits@crepuscular-aosp.icu>
2024-01-12 20:29:33 +0800

    pkgs: r3playx: initial build configuration
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

---
+++ pkgs/r3playx/default.nix
@@ -0,0 +1,125 @@
+{ lib
+, stdenv
+, fetchurl
+, undmg
+, dpkg
+, expat
+, autoPatchelfHook
+, wrapGAppsHook
+, makeWrapper
+, alsa-lib
+, at-spi2-atk
+, cairo
+, cups
+, nspr
+, nss
+, mesa # for libgbm
+, pango
+, xorg
+, xdg-utils
+, libdrm
+, libnotify
+, libsecret
+, libuuid
+, libxcb
+, libxkbcommon
+, gtk3
+, systemd
+}:
+let
+  pname = "r3playx";
+  version = "2.7.2";
+
+  srcs = {
+    x86_64-linux = fetchurl {
+      url = "https://github.com/EndCredits/music/releases/download/2.7.2/R3PLAYX-2.7.2-linux.deb";
+      hash = "sha256-e/ZdaaQDVvJOgQ3Sv3qihfO+uh6zLe1d4dYERVtq2No=";
+    };
+    x86_64-darwin = fetchurl {
+      url = "https://github.com/Sherlockouo/music/releases/download/${version}/R3PLAYX-${version}-mac-x64.dmg";
+      hash = "sha256-z8CASZRWKlj1g3mhxTMMeR4klTvQ2ReSrL7Rt18qQbM=";
+    };
+    aarch64-darwin = fetchurl {
+      url = "https://github.com/Sherlockouo/music/releases/download/${version}/R3PLAYX-${version}-mac-arm64.dmg";
+      hash = "sha256-McYLczudKG4tRNIw/Ws4rht0n4tiKA2M99yKtJbdlY8=";
+    };
+  };
+  src = srcs.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+
+  libraries = [
+    alsa-lib
+    at-spi2-atk
+    cups
+    expat
+    nspr
+    nss
+    mesa
+    pango
+    xorg.libxshmfence
+    xorg.libXScrnSaver
+    xorg.libXtst
+    xdg-utils
+    libdrm
+    libnotify
+    libsecret
+    libuuid
+    libxcb
+    libxkbcommon
+    gtk3
+  ];
+
+  meta = with lib; {
+    description = "A music player forked from YesPlayMusic";
+    homepage = "https://github.com/Sherlockouo/music";
+    license = licenses.agpl3Only;
+    maintainers = with maintainers; [ ChaosAttractor ];
+    platforms = builtins.attrNames srcs;
+  };
+in
+if stdenv.isDarwin
+then stdenv.mkDerivation {
+  inherit pname version src meta;
+
+  nativeBuildInputs = [ undmg ];
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    mkdir -p $out/Applications
+    cp -r *.app $out/Applications
+  '';
+}
+else stdenv.mkDerivation {
+  inherit pname version src meta;
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    wrapGAppsHook
+    makeWrapper
+  ];
+
+  buildInputs = libraries;
+
+  runtimeDependencies = [
+    (lib.getLib systemd)
+  ];
+
+  unpackPhase = ''
+    ${dpkg}/bin/dpkg-deb -x $src .
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp -r opt $out/opt
+    cp -r usr/share $out/share
+    substituteInPlace $out/share/applications/desktop.desktop \
+      --replace "/opt/R3PLAYX/desktop" "$out/bin/desktop"
+    makeWrapper $out/opt/R3PLAYX/desktop $out/bin/desktop \
+      --argv0 "r3playx" \
+      --add-flags "$out/opt/R3PLAYX/resources/app.asar"
+
+    runHook postInstall
+  '';
+}
